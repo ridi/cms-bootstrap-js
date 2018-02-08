d33a4ad992fe9c9e4b01936955722c305104bc7e
Jihun Kim <devgrapher@ridi.com>
2018-02-08 17:43:30 +0900

    Refactor sample

--- index.js
+++ index.js
@@ -1,56 +1,50 @@
 require('babel-polyfill');
 const express = require('express');
 const Cookies = require('cookies');
-const { CmsSdk, CmsSession } = require('@ridi/cms-sdk');
+const { CmsSdk, LoginSession } = require('@ridi/cms-sdk');
 
 const PORT = 8080;
 const RPC_END_POINT = 'http://localhost';
-
 
 const sdk = new CmsSdk({
   cmsRpcUrl: RPC_END_POINT,
 });
 
-
 async function authorizer(req, res, next) {
   console.log(req.url);
 
-  req.session = new CmsSession(sdk);
+  req.session = new LoginSession(sdk);
   const cookies = new Cookies(req, null);
   const token = cookies.get('cms-token');
 
-  const data = await req.session.shouldRedirectForLogin(token);
-
-  if (!data || !data.user_id) {
-    const loginUrl = sdk.getLoginPageUrl(req.url);
-    res.redirect(req.baseUrl + loginUrl);
-    return;
-  }
-
-  const allowed = await req.session.authorizeUrl(req.method, req.url);
-  if (allowed) {
+  try {
+    await req.session.authorize(token, req.url);
     console.log(`access allowed: ${req.url}`);
     next();
-  } else {
-    res.sendStatus(403);
+  } catch (e) {
+    console.log(e);
+    const loginUrl = sdk.getLoginPageUrl(req.url);
+    res.redirect(req.baseUrl + loginUrl);
   }
 }
-
 
 const app = express();
 
 app.use(authorizer);
 
 app.get('/example/home', async (req, res) => {
+  res.send(`Hello! ${req.session.getLoginId()}.`);
+});
+
+app.get('/example/user/menus', async (req, res) => {
   const menus = await req.session.getUserMenus();
   res.json(menus);
 });
 
-// Forbidden
-app.get('/example/', (req, res) => {
-  res.send(req.session.getLoginId());
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
